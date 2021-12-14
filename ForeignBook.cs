57ba01aa3deca19fd48a881166480cc38e683746
Valeria Shevchenko <valeriya-shevchenko-2002@mail.ru>
2021-12-14 11:54:25 +0700

    Создание производного класса ForeignBook

---
+++ ForeignBook.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+	class ForeignBook : Book
+	{
+		private String language;
+
+		public ForeignBook()
+		{
+			language = "Английский";
+		}
+
+		public ForeignBook(String language)
+		{
+			this.language = language;
+		}
+	}
+}
