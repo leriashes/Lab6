a0ae09f965060d1e99417b0b14d18303335dee99
Valeria Shevchenko <valeriya-shevchenko-2002@mail.ru>
2021-12-14 12:06:03 +0700

    Перегрузка базового метода в производном классе

--- ForeignBook.cs
+++ ForeignBook.cs
@@ -29,5 +29,26 @@ namespace Lab6
 		{
 			this.language = language;
 		}
+
+		public void Init(String title, Author author, int pages_number, String genre, Publishing publishing, int publication_year, String language)
+		{
+			this.title = title;
+			this.author = author;
+			this.pages_number = Math.Abs(pages_number);
+			this.genre = genre;
+			this.publishing = publishing;
+			this.publ_year = Math.Abs(publication_year);
+			this.language = language;
+
+			return;
+		}
+
+		public void Init(String title, Author author, int pages_number, String genre, Reader reader, Publishing publishing, int publication_year, String language)
+		{
+			this.Init(title, author, pages_number, genre, reader, publishing, publication_year);
+			this.language = language;
+
+			return;
+		}
 	}
 }
