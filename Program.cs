c8a65d23250c4c067736caa014d6cec7f32db8ce
Valeria Shevchenko <valeriya-shevchenko-2002@mail.ru>
2021-12-14 12:06:03 +0700

    Перегрузка базового метода в производном классе

--- Program.cs
+++ Program.cs
@@ -642,6 +642,17 @@ namespace Lab6
 
 			Console.Write($"\n\ndvum_mas[{k},{d}]");
 			dvum_mas[k,d].Display();
+
+			Date fdate = new Date(28, 11, 2001);
+			Address faddress = new Address();
+			Reader freader = new Reader("Шишкова Валентина Алексеевна", fdate, faddress, 123456789);
+			Author fauthor = new Author("Сьюэлл Анна", date_1, "Великобритания");
+			Publishing fpublishing = new Publishing();
+
+			ForeignBook fbook = new ForeignBook("Good Morning", fauthor, 345, "Фантастика", freader, fpublishing, 2021, "Английский");
+			ForeignBook fbook1 = new ForeignBook();
+
+			fbook1.Init("Black Beauty", fauthor, 289, "Рассказы", publishing_1, 2019, "Английский");
 		}
 	}
 }
